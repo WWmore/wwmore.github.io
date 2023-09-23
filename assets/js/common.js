2bc08c6feafc08db80ec5c4d09add3751446c42a
WWmore <i1103552872@gmail.com>
2023-09-23 15:39:19 +0300

    remove jupyter

--- assets/js/common.js
+++ assets/js/common.js
@@ -1,9 +1,25 @@
 $(document).ready(function() {
+    // add toggle functionality to abstract and bibtex buttons
     $('a.abstract').click(function() {
-        $(this).parent().parent().find(".abstract.hidden").toggleClass('open');
+      $(this).parent().parent().find(".abstract.hidden").toggleClass('open');
+      $(this).parent().parent().find(".bibtex.hidden.open").toggleClass('open');
     });
     $('a.bibtex').click(function() {
-        $(this).parent().parent().find(".bibtex.hidden").toggleClass('open');
+      $(this).parent().parent().find(".bibtex.hidden").toggleClass('open');
+      $(this).parent().parent().find(".abstract.hidden.open").toggleClass('open');
     });
-    $('.navbar-nav').find('a').removeClass('waves-effect waves-light');
-});
+    $('a').removeClass('waves-effect waves-light');
+  
+    // bootstrap-toc
+    if($('#toc-sidebar').length){
+      var navSelector = "#toc-sidebar";
+      var $myNav = $(navSelector);
+      Toc.init($myNav);
+      $("body").scrollspy({
+        target: navSelector,
+      });
+    }
+
+  });
+  
+  
